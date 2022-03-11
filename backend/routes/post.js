dead7a5a8c7ad731d36666661d0e72c67e374a63
Iacono Tristan <boulbi38220@gmail.com>
2022-03-11 14:30:17 +0100

    fin backend

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -10,7 +10,7 @@ const multer = require('../middleware/multer-config');
 /*** Routage de la ressource post */
 router.put("/update", auth, multer, postCtrl.update)
 router.post("/create", auth, multer, postCtrl.create);
-router.delete("/delete", postCtrl.delete)
+router.delete("/delete", auth, postCtrl.delete)
 router.get("/", auth, postCtrl.listMsg);
 
 module.exports = router; 
